7aa9ea79b1ca63485680e9dcea2bfcfd7eb24c1f
Anatoly Moskalev <moskalev1997tolya@gmail.com>
2022-03-22 14:57:22 +0600

    USD

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -22,7 +22,7 @@ class MobileBankApiTestV1 {
               .statusCode(200)
               // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
               .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-              .body("[0].currency", equalTo("RUR"));
+              .body("[0].currency", equalTo("USD"));
 
     }
 }
